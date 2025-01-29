06c2eb7d3a8e021ca8162ed2edae6d856c5ee565
BudenchukM <budenchuk.marina@gmail.com>
2025-01-29 14:24:57 +0300

    add bin/brain-games.js src/cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { cli } from 'src\cli.js';
+import cli from '../index.js';
 
 console.log("Welcome to the Brain Games!");
 cli();
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-export const cli = () => {
+export default () => {
     let userName = readlineSync.question('May I have your name? ');
     return console.log('Hello,' + userName + '!');
 };
