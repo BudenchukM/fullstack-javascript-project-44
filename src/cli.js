d3307a5dbe23de5d57ba8c4477a4c79311bf4fb1
BudenchukM <budenchuk.marina@gmail.com>
2025-01-29 13:22:05 +0300

    add bin/brain-games.js src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 export const cli = () => {
-    var userName = readlineSync.question('May I have your name? ');
+    let userName = readlineSync.question('May I have your name? ');
     return console.log('Hello,' + userName + '!');
 };
