e0dab42e56a52c5d98e1f7d7b286801f2769453e
BudenchukM <budenchuk.marina@gmail.com>
2025-01-31 13:58:19 +0300

    add games/even.js

---
+++ src/games/even.js
@@ -0,0 +1,12 @@
+import { randomNumber } from '../utils.js';
+import runEngine from '../index-even.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no"';
+
+export const generateRound = () => {
+  const question = randomNumber(1, 100);
+  const answer = question % 2 === 0 ? 'yes' : 'no';
+  return { answer, question };
+};
+
+export default () => runEngine(description, generateRound);
