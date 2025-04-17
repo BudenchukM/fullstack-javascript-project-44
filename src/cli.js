771cbe3fcd0326f2061a094b6cfb7f4def2b017c
BudenchukM <budenchuk.marina@gmail.com>
2025-04-17 17:59:35 +0300

    add -

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   const userName = readlineSync.question('May I have your name? ');
-  return console.log(`Hello,${userName}!`);
+  return console.log(`Hello, ${userName}!`);
 };
